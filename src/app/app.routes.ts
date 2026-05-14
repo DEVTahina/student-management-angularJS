b54c50df8d06b1e200b2f4426cc92362b2e2cde3
Tahiana <tahiana@gmail@.com>
2026-05-14 04:05:44 +0300

    Mise à jour codes ddu queleque ficheir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/components/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'students', pathMatch: 'full' },
+      {
+        path: 'students',
+        loadChildren: () =>
+          import('./features/students/students.routes').then((m) => m.routes),
+      },
+    ],
+  },
+
+  { path: '**', redirectTo: 'students' }
+];
